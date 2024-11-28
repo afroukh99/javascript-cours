3eb068f43b658e8aaa32a119b299546eeaf8cf21
afroukh99 <mohammedtea22@gmail.com>
2024-11-28 18:15:04 +0100

    tp-Traffic light

---
+++ tp4/index.js
@@ -0,0 +1,25 @@
+var redLight = document.getElementById("light-red")
+var orangeLight = document.getElementById("light-orange")
+var greenLight = document.getElementById("light-green")
+
+function switchLight(event) {
+    light = event.target.innerText;
+    switch (light) {
+        case "Red":
+            redLight.style.backgroundColor = light;
+            greenLight.style.backgroundColor = "";
+            orangeLight.style.backgroundColor = "";
+            break;
+        case "Orange":
+            orangeLight.style.backgroundColor = light;
+            greenLight.style.backgroundColor = "";
+            redLight.style.backgroundColor = "";
+            break;
+        case "Green":
+            greenLight.style.backgroundColor = light;
+            redLight.style.backgroundColor = "";
+            orangeLight.style.backgroundColor = "";
+            break;
+        default:
+    }
+}
